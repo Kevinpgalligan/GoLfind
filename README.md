f263c633a9bfd34217a2a4c5ea9fb50701f38d3b
kevinpgalligan <galligankevinp@gmail.com>
2019-12-23 15:01:56 +0000

    Allow animation of particular life instance.

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 ## Mona Lisa Game of Life
-Finding Game of Life states that eventually turn into pictures.
+Searching for Game of Life states that eventually turn into a given picture, such as the Mona Lisa.
 
 \<Insert demonstration gif here>
 
 #### TO-DO
-1. Develop animation stuff: pass a life; save sketch?; set frame rate; play for a certain number of frames.
+1. Use SKIPPY to save GIF of life.
 2. Convert GoL to SAT equation, pass to SAT solver. Gardens of Eden seem rare / weird-looking enough that it should be possible. The resulting SAT might be too big for SAT, or might not. Probably not.
 3. Write a function that does it N times.
 4. Run it on smiley face / Mona Lisa / whatever, save result.
 5. Write it up.
-
--- src/render.lisp
+++ src/render.lisp
@@ -1,14 +1,24 @@
 (in-package mona-lisa-gol)
 
 (defsketch run-life
-    ((life (random-life 50 100))
-     (pixels-per-cell 6)
-     (width (* 200 pixels-per-cell))
-     (height (* 100 pixels-per-cell)))
-  (setf life (life-next-state life))
+    ((initial-life (random-life 50 100))
+     ;; must override initial-life when passing life
+     ;; to the sketch.
+     ;; directly setting the default life to random
+     ;; doesn't allow it to be overridden, for some
+     ;; reason.
+     (life initial-life)
+     (pixels-per-cell 50)
+     (width (* (life-cols life) pixels-per-cell))
+     (height (* (life-rows life) pixels-per-cell))
+     (frames-per-state 50)
+     (i 0))
+  (incf i)
+  (when (= 0 (rem i frames-per-state))
+    (setf life (life-next-state life)))
   (background +white+)
-  (dotimes (row 100)
-    (dotimes (col 200)
+  (dotimes (row (life-rows life))
+    (dotimes (col (life-cols life))
       (with-pen (make-pen :fill (if (equalp DEAD (life-get-cell life row col))
                                     +white+
                                     +black+))
@@ -17,12 +27,13 @@
               pixels-per-cell
               pixels-per-cell)))))
 
-#|
-(run-life (life-from-lists (list (list DEAD LIVE LIVE LIVE LIVE LIVE DEAD)
-                                 (list LIVE DEAD DEAD DEAD DEAD DEAD LIVE)
-                                 (list LIVE DEAD LIVE DEAD LIVE DEAD LIVE)
-                                 (list LIVE DEAD DEAD DEAD DEAD DEAD LIVE)
-                                 (list LIVE DEAD DEAD DEAD DEAD DEAD LIVE)
-                                 (list DEAD LIVE LIVE LIVE LIVE LIVE DEAD)))
-          50)
-|#
+(defparameter +smiley+
+  (life-from-lists
+   (list
+    (list DEAD LIVE LIVE LIVE LIVE LIVE DEAD)
+    (list LIVE DEAD DEAD DEAD DEAD DEAD LIVE)
+    (list LIVE DEAD LIVE DEAD LIVE DEAD LIVE)
+    (list LIVE DEAD DEAD DEAD DEAD DEAD LIVE)
+    (list LIVE DEAD LIVE LIVE LIVE DEAD LIVE)
+    (list LIVE DEAD DEAD DEAD DEAD DEAD LIVE)
+    (list DEAD LIVE LIVE LIVE LIVE LIVE DEAD))))
